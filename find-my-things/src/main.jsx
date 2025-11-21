8a56ead5f9aee8cf7e7b4162fd3519f33d999410
lucashfs <lucashfsdev@gmail.com>
2025-11-20 23:35:43 -0300

    adjust: tips updated.

--- find-my-things/src/main.jsx
+++ find-my-things/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import LoginPage from './LoginPage'
 import MyThings from './components/MyThings'
 import ItemLocationPage from './pages/ItemLocationPage'
+import ConfiguracoesAjuda from './components/ConfiguracoesAjuda'
 import './assets/main.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ItemLocationPage />
+    <ConfiguracoesAjuda />
   </StrictMode>,
 )
