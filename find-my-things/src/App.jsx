150fce3fb9cc9aa8b03429fb1227d6b29ba9aad6
lucashfs <lucashfsdev@gmail.com>
2025-11-20 23:49:49 -0300

    feat: react router implemented.

--- find-my-things/src/App.jsx
+++ find-my-things/src/App.jsx
@@ -1,12 +1,18 @@
-import LoginPage from './LoginPage'
-import MyThings from './components/MyThings'
-import LocationPage from './pages/LocationPage'
-function App() {
-  return (
-    <>
-    <LocationPage/>
-    </>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default App
+import LoginPage from "./pages/LoginPage";
+import MyThings from "./pages/MyThingsPage";
+import ItemLocationPage from "./pages/ItemLocationPage";
+import SettingsAndSupportPage from "./pages/SettingsAndSupportPage"
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/minhas-coisas" element={<MyThings />} />
+        <Route path="/localizacao" element={<ItemLocationPage />} />
+        <Route path="/configuracao-ajuda" element={<SettingsAndSupportPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- find-my-things/src/main.jsx
+++ find-my-things/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LoginPage from './LoginPage'
-import MyThings from './components/MyThings'
-import ItemLocationPage from './pages/ItemLocationPage'
-import ConfiguracoesAjuda from './components/ConfiguracoesAjuda'
+import App from './App'
 import './assets/main.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConfiguracoesAjuda />
+    <App />
   </StrictMode>,
 )
